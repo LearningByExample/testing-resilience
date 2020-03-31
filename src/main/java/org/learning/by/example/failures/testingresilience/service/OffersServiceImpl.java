5747f51fe556cbf66ee7816b868f10b068d5f670
Juan Medina <jamedina@gmail.com>
2020-03-31 23:04:21 +0100

    adding fallback

--- src/main/java/org/learning/by/example/failures/testingresilience/service/OffersServiceImpl.java
+++ src/main/java/org/learning/by/example/failures/testingresilience/service/OffersServiceImpl.java
@@ -2,19 +2,53 @@ package org.learning.by.example.failures.testingresilience.service;
 
 import org.learning.by.example.failures.testingresilience.repository.Offer;
 import org.learning.by.example.failures.testingresilience.repository.OffersRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
 @Component
 public class OffersServiceImpl implements OffersService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OffersServiceImpl.class);
+    private final ConcurrentLinkedDeque<Offer> offersFallBack;
     private final OffersRepository offersRepository;
 
     public OffersServiceImpl(final OffersRepository offersRepository) {
         this.offersRepository = offersRepository;
+        offersFallBack = new ConcurrentLinkedDeque<>();
+    }
+
+    public void prepareFallBack() {
+        final List<Offer> offers = offersRepository.findAll()
+            .onErrorResume(throwable -> {
+                LOGGER.warn("error getting fallback offers", throwable);
+                return Flux.empty();
+            })
+            .collectList().block();
+
+        if ((offers != null) && (offers.size() > 0)) {
+            clearFallBack();
+            offersFallBack.addAll(offers);
+        }
+    }
+
+    public void clearFallBack() {
+        offersFallBack.clear();
     }
 
     @Override
     public Flux<Offer> getOffers() {
-        return offersRepository.findAll();
+        return offersRepository.findAll().onErrorResume(ignore -> Flux.fromIterable(offersFallBack));
+    }
+
+    @Override
+    public boolean isReady() {
+        if (offersFallBack.size() == 0) {
+            prepareFallBack();
+        }
+        return offersFallBack.size() > 0;
     }
 }

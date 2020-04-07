e431f9abf54d5baf2a977b07b6f1e140eecbc4f8
Juan Medina <jamedina@gmail.com>
2020-04-07 08:12:55 +0100

    better local k8s test

--- src/test/java/org/learning/by/example/failures/testingresilience/LocalK8sTest.java
+++ src/test/java/org/learning/by/example/failures/testingresilience/LocalK8sTest.java
@@ -1,9 +1,11 @@
 package org.learning.by.example.failures.testingresilience;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.learning.by.example.failures.testingresilience.test.BaseK8sTest;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
 
 public class LocalK8sTest extends BaseK8sTest {
@@ -12,10 +14,11 @@ public class LocalK8sTest extends BaseK8sTest {
     }
 
     @Test
-    public void oneTest() throws Exception {
+    @DisplayName("We should test in K8s")
+    public void weShouldTestInK8s() throws Exception {
         if (podsExists()) {
             deletePods();
-            await().atMost(1, MINUTES).until(() -> !podsExists());
+            await().atMost(5, MINUTES).and().with().pollInterval(3, SECONDS).until(() -> !podsExists());
         }
         createDatabase();
     }
